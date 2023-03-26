2847b602c88f5c9b8c56f84fbc67f7ea79da1e30
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-03-26 15:11:04 -0300

    feat: list products - route - (ProductController)

--- api/src/main/java/com/backend/api/Product/ProductController/ProductController.java
+++ api/src/main/java/com/backend/api/Product/ProductController/ProductController.java
@@ -1,10 +1,21 @@
 package com.backend.api.Product.ProductController;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.backend.api.Product.ProductService.ProductService;
+import com.backend.api.Product.ProductModel.ProductModel;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+    
+    @GetMapping("/products")
+    public Iterable<ProductModel> products() {
+        return productService.listProducts();
+    }
+    
     @GetMapping("/")
     public String test() {
         return "test";
