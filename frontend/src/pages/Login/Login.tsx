3ee65ae20df8c73896b742ce23807f3185500ea0
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-03-27 12:10:13 -0300

    feat: beggining of component Login

---
+++ frontend/src/pages/Login/Login.tsx
@@ -0,0 +1,19 @@
+import styles from './Login.module.css';
+
+interface ILogin{
+    email: string;
+    password: string;
+}
+
+const Login = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Login</h1>
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+        </div>
+    );
+};
+
+export default Login;
