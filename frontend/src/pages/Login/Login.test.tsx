485e64fb1788bc5871b86bd37891b5f1ce9b18b7
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-03-29 09:22:32 -0300

    feat: test Login - Login.test.tsx

--- frontend/src/pages/Login/Login.test.tsx
+++ frontend/src/pages/Login/Login.test.tsx
@@ -1,1 +1,19 @@
 // react testing library
+import { render, screen } from '@testing-library/react'
+import { expect } from 'jest-without-globals'
+import { describe, it } from 'vitest'
+import Login from './Login'
+
+
+// import the component to be tested
+describe("<Login />", () => {
+    it('renders the title', () => {
+
+        // render the component
+        render(<Login />);
+        // toBeInTheDocument() is a method from jest-dom
+        expect(screen.getByText('Login')).toBeInTheDocument();
+    });
+});
+
+
