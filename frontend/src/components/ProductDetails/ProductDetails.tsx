f4639a20027520f35d48aa42f5ca2acc9a1453f6
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-04-19 00:45:33 -0300

    feat: component ProductDetails

---
+++ frontend/src/components/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+    const { id } = useParams();
+    return <div>Product Details {id}</div>;
+    }
+
+export default ProductDetails;
