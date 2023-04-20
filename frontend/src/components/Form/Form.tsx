1d801eace76b3b97d23640bdefa4206513237953
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-04-20 12:11:59 -0300

    feat: component Form cleaning fields

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -2,27 +2,36 @@ import styles from "./Form.module.css"
 import { useContext,useState } from "react";
 import contextProduct from "../../context/contextProduct";
 import apiBackend from "../../services/api-backend/api-backend";
+
 interface IForm{
     register: boolean;
 }
 
 const Form = ({register}: IForm) => {
+    const defaultProduct = useContext(contextProduct);
     const [objProduct, setObjProduct] = useState(useContext(contextProduct));
 
     const typing = (e: any) => {
         setObjProduct({...objProduct, [e.target.name]: e.target.value});
     }
 
+    const clearFields = () => {
+        setObjProduct(defaultProduct);
+    }
+
+
     const registerProduct = () => {
         apiBackend.postProduct(objProduct.name, objProduct.description);
+        alert("Product registered");
+        clearFields();
     }
 
     return(
         <>
             <p>{JSON.stringify(objProduct)}</p>
             <form className={styles.form}>
-                <input type="text" onChange={typing} name="name" placeholder="Name" className="form-control"/>
-                <input type="text" onChange={typing} name="description" placeholder="Description" className="form-control"/>
+                <input type="text" onChange={typing} name="name" placeholder="Name" className="form-control" value={objProduct.name}/>
+                <input type="text" onChange={typing} name="description" placeholder="Description" className="form-control" value={objProduct.description}/>
                 {
                     register 
                     ?
